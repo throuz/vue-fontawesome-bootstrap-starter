4b6b5e0cecb8a7e1207d9e4313f38ec78c70c63b
Throuz <zed2786328@gmail.com>
2025-01-07 22:25:50 +0800

    fontawesome setup

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,10 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div class="test text-primary">I'm test SCSS</div>
+    <div class="test text-primary">
+      I'm test SCSS
+      <font-awesome-icon icon="fa-solid fa-dog" />
+    </div>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
