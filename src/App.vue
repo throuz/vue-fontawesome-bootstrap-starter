4684602bffca91555f517809deabc58ee26a5dc8
Throuz <zed2786328@gmail.com>
2025-01-09 11:59:01 +0800

    optimize style

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div class="test text-primary">
-      I'm test SCSS
-      <font-awesome-icon icon="fa-solid fa-dog" />
-    </div>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <div class="test text-primary">
+        I'm test SCSS
+        <font-awesome-icon icon="fa-solid fa-dog" />
+      </div>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
